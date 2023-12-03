63b3f195e6d0713ff2a4529e379ef700517d6ecd
seigetsu <nyarlathotep44@gmail.com>
2023-12-04 00:02:03 +0900

    サインインページのコンポーネントに対するテストを作成

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("sign in", () => {
+  render(<SignInPage />);
+  const emailForm = screen.getByPlaceholderText("email");
+  expect(emailForm).toBeInTheDocument();
+  const passwordForm = screen.getByPlaceholderText("password");
+  expect(passwordForm).toBeInTheDocument();
+  const singinButton = screen.getByText("サインイン");
+  expect(singinButton).toBeInTheDocument();
+  const emailLabel = screen.getByText("メールアドレス");
+  expect(emailLabel).toBeInTheDocument();
+  const passwordLabel = screen.getByText("パスワード");
+  expect(passwordLabel).toBeInTheDocument();
 });
